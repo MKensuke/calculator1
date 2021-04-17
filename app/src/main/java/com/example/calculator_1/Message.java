db7ddcb972b30ca00cef30af981988261cde867c
Miyawaki <mkandthensome@gmail.com>
2021-04-18 05:31:03 +0900

    Messageクラス追加

---
+++ app/src/main/java/com/example/calculator_1/Message.java
@@ -0,0 +1,20 @@
+package com.example.calculator_1;
+
+//メッセージを管理するクラス、エラーメッセージも通常メッセージもここで管理
+public enum Message {
+    MSG001 {
+        @Override public String toString() {
+            return "数字を二つ入力してください";
+        }
+    },
+    MSG002 {
+        @Override public String toString() {
+            return "計算記号を選択してください";
+        }
+    },
+    MSG003 {
+        @Override public String toString() {
+            return "0除算はできません";
+        }
+    },
+}
