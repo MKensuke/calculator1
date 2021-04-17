823c911db105de13f40122729607745677d16d4b
Miyawaki <mkandthensome@gmail.com>
2021-04-18 06:02:28 +0900

    記述方法の簡潔化と計算式の修正

--- app/src/main/java/com/example/calculator_1/Calculation.java
+++ app/src/main/java/com/example/calculator_1/Calculation.java
@@ -4,19 +4,15 @@ public class Calculation {
     int calculateResult;
 
     private int add(int inputNum1, int inputNum2){
-        calculateResult = inputNum1 + inputNum2;
-        return calculateResult;
+        return calculateResult = inputNum1 + inputNum2;
     }
     private int subtract(int inputNum1, int inputNum2){
-        calculateResult = inputNum1 - inputNum2;
-        return calculateResult;
+        return calculateResult = inputNum1 - inputNum2;
     }
     private int multiply(int inputNum1, int inputNum2){
-        calculateResult = inputNum1 + inputNum2;
-        return calculateResult;
+        return calculateResult = inputNum1 * inputNum2;
     }
     private int divide(int inputNum1, int inputNum2){
-        calculateResult = inputNum1 + inputNum2;
-        return calculateResult;
+        return calculateResult = inputNum1 / inputNum2;
     }
 }
