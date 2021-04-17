6743f890aec76abd12b07784e5eb13b0a003db27
Miyawaki <mkandthensome@gmail.com>
2021-04-18 02:58:38 +0900

    とりあえずFIZZBUZZから転用

--- app/src/main/java/com/example/calculator_1/MainActivity.java
+++ app/src/main/java/com/example/calculator_1/MainActivity.java
@@ -1,56 +1,87 @@
 package com.example.calculator_1;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.text.SpannableStringBuilder;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
-
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // ボタン
+        Button button = findViewById(R.id.button1);
+
+        button.setOnClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onClick(View v) {
+        //出力値
+        String strOUTFIZZBUZZ = "";
+        //入力値
+        int iInputnum;
+        //入力値FIZZ値
+        int iInputFIZZ;
+        //入力値BUZZ値
+        int iInputBUZZ;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        int iFizzFlag = 0;
+        int iBuzzFlag = 0;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // 数値FIZZBUZZを表示するテキスト
+        TextView textView = findViewById(R.id.textView1);
+        //　入力値
+        EditText editText = findViewById(R.id.editText1);
+        SpannableStringBuilder sb = (SpannableStringBuilder) editText.getText();
+        String string1 = sb.toString();
+        iInputnum = Integer.parseInt(string1);
+        //　FIZZ値
+        EditText editTextFIZZ = findViewById(R.id.editTextFIZZ);
+        SpannableStringBuilder sbfizz = (SpannableStringBuilder) editTextFIZZ.getText();
+        String stringfizz = sbfizz.toString();
+        iInputFIZZ = Integer.parseInt(stringfizz);
+        //　BUZZ値
+        EditText editTextBUZZ = findViewById(R.id.editTextBUZZ);
+        SpannableStringBuilder sbbuzz = (SpannableStringBuilder) editTextBUZZ.getText();
+        String stringbuzz = sbbuzz.toString();
+        iInputBUZZ = Integer.parseInt(stringbuzz);
+
+        //フラグ設定
+        if (0 == (iInputFIZZ % iInputnum)) {
+            iFizzFlag = 1;
         }
 
-        return super.onOptionsItemSelected(item);
+        if (0 == (iInputBUZZ % iInputnum)) {
+            iBuzzFlag = 1;
+        }
+
+        // iFizzFlagが立っている場合、FIZZを設定
+        if (1 == iFizzFlag) {
+            strOUTFIZZBUZZ = strOUTFIZZBUZZ + "FIZZ";
+        }
+
+        // iBuzzFlagが立っている場合、BUZZを設定
+        if (1 == iBuzzFlag) {
+            strOUTFIZZBUZZ = strOUTFIZZBUZZ + "BUZZ";
+        }
+
+        // フラグ立ってない場合、出力値に入力した値を設定
+        if ((0 == iFizzFlag) && (0 == iBuzzFlag)) {
+            strOUTFIZZBUZZ = String.valueOf(iInputnum);
+        }
+        // strOUTFIZZBUZZをテキストを設定して表示
+        textView.setText(strOUTFIZZBUZZ);
+
+//        FIZZBUZZ fizzbuzz = new FIZZBUZZ(iInputFIZZ, iInputBUZZ);
+//
+//        strOUTFIZZBUZZ = fizzbuzz.FIZZBUZZStrChk(iInputnum);
     }
 }
