8846b0c80a9ded4b25e5d8c33548e7019ef96597
Miyawaki <mkandthensome@gmail.com>
2021-04-18 05:59:47 +0900

    Calculationクラス追加

---
+++ app/src/main/java/com/example/calculator_1/Calculation.java
@@ -0,0 +1,22 @@
+package com.example.calculator_1;
+
+public class Calculation {
+    int calculateResult;
+
+    private int add(int inputNum1, int inputNum2){
+        calculateResult = inputNum1 + inputNum2;
+        return calculateResult;
+    }
+    private int subtract(int inputNum1, int inputNum2){
+        calculateResult = inputNum1 - inputNum2;
+        return calculateResult;
+    }
+    private int multiply(int inputNum1, int inputNum2){
+        calculateResult = inputNum1 + inputNum2;
+        return calculateResult;
+    }
+    private int divide(int inputNum1, int inputNum2){
+        calculateResult = inputNum1 + inputNum2;
+        return calculateResult;
+    }
+}
